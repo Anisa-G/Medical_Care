88e04187bc572c3734eba5909720c2531c32a5e3
marselkasa <37622092+marselkasa@users.noreply.github.com>
2018-04-06 18:52:11 +0200

    Geo location added to class hospital-map shife olti

--- src/pages/hospital-map/hospital-map.ts
+++ src/pages/hospital-map/hospital-map.ts
@@ -54,11 +54,13 @@ export class HospitalMapPage {
   
  
 
-    var map = new google.maps.Map(document.getElementById('map'), {
+    var infoWindow,map = new google.maps.Map(document.getElementById('map'), {
       zoom: 18,
       center: location
 
     });
+    infoWindow = new google.maps.InfoWindow;
+    
     this.image = 'assets/icon/custom-marker.png'
     var marker = new google.maps.Marker({
       position: location,
@@ -82,7 +84,34 @@ export class HospitalMapPage {
         directionsDisplay.setDirections(response);
       }
     });
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+  
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('Location found.');
+        infoWindow.open(map);
+        map.setCenter(pos);
+      }, function() {
+        handleLocationError(true, infoWindow, map.getCenter());
+      });
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+        }
+    }
   }
-}
+  
+
 
 

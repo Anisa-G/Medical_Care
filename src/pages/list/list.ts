37f303176558087aab73914f76372394b84a4369
Olti <beqja.96@gmail.com>
2018-06-09 12:19:01 +0200

    geting the list of spitals from the json

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import 'rxjs/add/operator/map';
+import { Http } from '@angular/http';
 //, ViewChild, OnInit, Renderer
 //import { NavController, NavParams, CardContent } from 'ionic-angular';
 
@@ -12,8 +14,34 @@ import { HospitalMapPage } from '../hospital-map/hospital-map';
 })
 export class ListPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+  listOfQytete: any[];
+  listOfSpitale: any[];
+  listOfDepatamente: any[];
+
+  constructor(public navParams: NavParams, private http: Http) {
     
+    let localData = this.http.get("assets/json/list_spitale.json").map(res => res.json());
+    console.log(localData)
+    localData.subscribe(data => {
+      // console.log(data);
+      this.listOfQytete =Object.keys(data);
+      console.log(this.listOfQytete);
+    });
   }
+
+
+  optionDepartamente(item) {
+    console.log(item)
+  }
+
+  optionSpitale(item) {
+    console.log(item)
+  }
+
+  optionQytete(item) {
+    console.log(item)
+  }
+
 }
 
